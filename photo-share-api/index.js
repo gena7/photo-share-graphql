458c2cf4ed60a4e160467f9bf7cc026f39ae8997
gena7 <gena7hh@gmail.com>
2020-11-17 12:26:42 +0900

    Add many to many fields

--- photo-share-api/index.js
+++ photo-share-api/index.js
@@ -6,6 +6,7 @@ const typeDefs = `
     name: String!
     avatar: String
     postedPhotos: [Photo!]!
+    inPhotos: [Photo!]!
   }
 
   enum PhotoCategory {
@@ -23,6 +24,7 @@ const typeDefs = `
     description: String
     category: PhotoCategory!
     postedBy: User!
+    taggedUsers: [User!]!
   }
 
   input PostPhotoInput {
@@ -72,6 +74,13 @@ const photos = [
   },
 ];
 
+const tags = [
+  { photoID: "1", userID: "gPlake" },
+  { photoID: "2", userID: "sSchmidt" },
+  { photoID: "2", userID: "mHattrup" },
+  { photoID: "2", userID: "gPlake" },
+];
+
 const resolvers = {
   Query: {
     totalPhotos: () => photos.length,
@@ -93,11 +102,21 @@ const resolvers = {
     postedBy: (parent) => {
       return users.find((u) => u.githubLogin === parent.githubUser);
     },
+    taggedUsers: (parent) =>
+      tags
+        .filter((tag) => tag.photoID === parent.id)
+        .map((tag) => tag.userID)
+        .map((userID) => users.find((u) => u.githubLogin === userID)),
   },
   User: {
     postedPhotos: (parent) => {
       return photos.filter((p) => p.githubUser === parent.githubLogin);
     },
+    inPhotos: (parent) =>
+      tags
+        .filter((tag) => tag.userID === parent.id)
+        .map((tag) => tag.photoID)
+        .map((photoID) => photos.find((p) => p.id === photoID)),
   },
 };
 
