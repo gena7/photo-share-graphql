e9b0d93c01f6bb9120f5acd6dc7ef7e0ab59f7a9
gena7 <gena7hh@gmail.com>
2020-11-17 12:01:24 +0900

    Add one to one fields

--- photo-share-api/index.js
+++ photo-share-api/index.js
@@ -1,6 +1,13 @@
 const { ApolloServer } = require("apollo-server");
 
 const typeDefs = `
+  type User {
+    githubLogin: ID!
+    name: String!
+    avatar: String
+    postedPhotos: [Photo!]!
+  }
+
   enum PhotoCategory {
     SELFIE
     PORTRAIT
@@ -15,6 +22,7 @@ const typeDefs = `
     name: String!
     description: String
     category: PhotoCategory!
+    postedBy: User!
   }
 
   input PostPhotoInput {
@@ -34,7 +42,35 @@ const typeDefs = `
 `;
 
 let _id = 0;
-const photos = [];
+
+const users = [
+  { githubLogin: "mHattrup", name: "Mike Hattrup" },
+  { githubLogin: "gPlake", name: "Glen Plake" },
+  { githubLogin: "sSchmidt", name: "Scot Schmidt" },
+];
+
+const photos = [
+  {
+    id: "1",
+    name: "Dropping the Heart Chute",
+    description: "The heart chute is one of my favorite chutes",
+    category: "ACTION",
+    githubUser: "gPlake",
+  },
+  {
+    id: "2",
+    name: "Enjoying the sunshine",
+    category: "SELFIE",
+    githubUser: "sSchmidt",
+  },
+  {
+    id: "3",
+    name: "Gunbarrel 25",
+    description: "25 laps on gunbarrel today",
+    category: "LANDSCAPE",
+    githubUser: "sSchmidt",
+  },
+];
 
 const resolvers = {
   Query: {
@@ -54,6 +90,14 @@ const resolvers = {
   },
   Photo: {
     url: (parent) => `http://example.com/img/${parent.id}.jpg`,
+    postedBy: (parent) => {
+      return users.find((u) => u.githubLogin === parent.githubUser);
+    },
+  },
+  User: {
+    postedPhotos: (parent) => {
+      return photos.filter((p) => p.githubUser === parent.githubLogin);
+    },
   },
 };
 
